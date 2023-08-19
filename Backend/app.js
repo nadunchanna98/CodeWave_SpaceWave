4b4b2199e664bbaa5222e2256323288188992ed2
nadunchanna98 <gancp1218@gmail.com>
2023-08-19 22:01:49 +0530

    sequelize cofiged

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,41 @@
+const cors = require("cors");
 const express = require("express");
-
-const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+const db = require("./models");
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+  //In development,  need to drop existing tables and re-sync database. 
+  db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  });
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to SpaceWave - Backend!!!" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server is up and running on port ${PORT}.`);
 });
