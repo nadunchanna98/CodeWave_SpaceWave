b2c0d8f0d14a0ad24c6a65e1ef864a1ee51ab1b8
Sachira Heshan <sachiraheshan99@gmail.com>
2023-08-20 15:42:11 +0530

    Home page and navigation

--- frontend/src/pages/navigation/AppStack.js
+++ frontend/src/pages/navigation/AppStack.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../user/HomePage';
+import Booking from '../user/Booking';
+import MyTrips from '../user/MyTrips';
+import Planets from '../user/Planets';
+import Plans from '../user/Plans';
+import About from '../user/About';
 
 function AppStack() {
   return (
     <Routes>
-             <Route exact  path="/" element={<Home/>} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/booking" element={<Booking />} />
+      <Route exact path="/my-trips" element={<MyTrips />} />
+      <Route exact path="/planets" element={<Planets />} />
+      <Route exact path="/plans" element={<Plans />} />
+      <Route exact path="/about" element={<About />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/user/Plans.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Plans.css'
+
+function Plans() {
+    return (
+        <div>
+            <div><h3 className='heading-text'>Plans</h3></div>
+        </div>
+    );
+}
+
+export default Plans;
